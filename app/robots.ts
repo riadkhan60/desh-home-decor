1b9692df023c7bc09f0f851210f362281768e011
riadkhan60 <khanriad60@gmail.com>
2026-03-05 01:05:48 +0600

    updated

--- app/robots.ts
+++ app/robots.ts
@@ -12,7 +12,6 @@ export default function robots(): MetadataRoute.Robots {
         '/admin-login',
         '/checkout/',
         '/api/',
-        '/_next/',
         '/debug.log',
       ],
     },
